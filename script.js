aa2017863efaa58ef8397db3ac05ab41f4a92fa9
Sarah-0911 <sarah.tabet69@gmail.com>
2024-12-13 16:52:01 +0100

    day 1 / part 2 ok

--- script.js
+++ script.js
@@ -1021,7 +1021,22 @@ const totalDistance = (leftList, rightList) => {
   for (let i = 0; i < leftList.length; i++) {
     total += Math.abs(leftList[i] - rightList[i]);
   }
-  console.log(total);
+  return total;
 }
 
-totalDistance(leftList, rightList)
+totalDistance(leftList, rightList) // Good answer !
+
+
+const totalSimilarityScore = (leftList, rightList) => {
+  let totalSimilarity = 0;
+  for (let i = 0; i < leftList.length; i++) {
+    for (let j = 0; j < rightList.length; j++) {
+      if (leftList[i] === rightList[j]) {
+        totalSimilarity += leftList[i];
+      }
+    }
+  }
+  console.log(totalSimilarity);
+}
+
+totalSimilarityScore(leftList, rightList) // Good answer !
