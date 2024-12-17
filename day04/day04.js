0dc49e46f96ebf13ff39ceb80e4283f755a5addb
Sarah-0911 <sarah.tabet69@gmail.com>
2024-12-17 18:11:46 +0100

    day 4 / part 1 in progress again

--- day04/day04.js
+++ day04/day04.js
@@ -1,32 +1,39 @@
 import { rawData04 } from "./rawData04.js"
 
+// const grid = rawData04.split("\n").map(row => row.split(''));
 
-const grid = rawData04.split("\n").map(row => row.split(''));
+const grid = [
+['M', 'M', 'M', 'S', 'A', 'M', 'X', 'A', 'M', 'M'],
+['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'],
+['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'],
+['M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X']
+]
 
-console.log(grid);
+// console.log(grid);
 
 const findXmas = () => {
   let totalXmas = 0;
 
-  grid.forEach((line) => {
-    for (let i = 0; i < line.length - 3; i++) {
-      let sequence = line.slice(i, i + 4).join('');
+  grid.forEach((row, index) => {
+    for (let i = 0; i < row.length - 3; i++) {
+      let sequence = row.slice(i, i + 4).join('');
       if (sequence === "XMAS" || sequence === "SAMX") {
         totalXmas++;
       }
     }
+    // console.log(row[1]);
   })
 
-  console.log(totalXmas);
+  // let columns = [];
+
+  // for (let i = 0; i < grid[0].length; i ++) {
+  //   columns.push([])
+  // }
+
+  // console.log(totalXmas);
 }
 
 findXmas()
 
 
 // dans une grid ya plusieurs tableaux les uns en dessous des autres, chacun comprenant plusieurs lettres.
-
-
-  // ['M', 'M', 'M', 'S', 'A', 'M', 'X', 'A', 'M', 'M'],
-  // ['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'],
-  // ['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'],
-  // ['M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X']
