3d736fb495e016ded09304110f5bd3768292a766
Sarah-0911 <sarah.tabet69@gmail.com>
2025-01-02 20:04:10 +0100

    day 4 / part 2 in progress

--- day04/day04.js
+++ day04/day04.js
@@ -1,20 +1,18 @@
 import { rawData04 } from "./rawData04.js"
 
-const grid = `MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX`.split("\n").map(row => row.split(''))
-
-console.log(grid.length);
+const grid = `
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........`.split("\n").map(row => row.split(''))
 
 const input = rawData04.split("\n").map(row => row.split(''));
-
 
 // const grid = [
 // ['M', 'X', 'M', 'A', 'S', 'M', 'X', 'X', 'M', 'M'],
@@ -29,6 +27,8 @@ const input = rawData04.split("\n").map(row => row.split(''));
 const findXmas = (grid) => {
   let total = 0;
 
+  //1ere partie
+
   const checkRight = (x, y) => {
     return  x + 3 < grid[0].length && y < grid.length &&
      grid[y][x + 1] === 'M' && grid[y][x + 2] === 'A' && grid[y][x + 3] === 'S';
@@ -40,52 +40,86 @@ const findXmas = (grid) => {
   }
 
   const checkDown = (x, y) => {
-    return  y + 3 < grid.length && x < grid[0].length &&
+    return y + 3 < grid.length && x < grid[0].length &&
      grid[y + 1][x] === 'M' && grid[y + 2][x] === 'A' && grid[y + 3][x] === 'S';
   }
 
   const checkUp = (x, y) => {
-    return  y - 3 >= 0 && x < grid[0].length &&
+    return y - 3 >= 0 && x < grid[0].length &&
      grid[y - 1][x] === 'M' && grid[y - 2][x] === 'A' && grid[y - 3][x] === 'S';
   }
 
   const checkRightDown = (x, y) => {
-    return  y + 3 < grid.length && x + 3 < grid[0].length &&
+    return y + 3 < grid.length && x + 3 < grid[0].length &&
      grid[y + 1][x + 1] === 'M' && grid[y + 2][x + 2] === 'A' && grid[y + 3][x + 3] === 'S';
   }
 
   const checkLeftUp = (x, y) => {
-    return  y - 3 >= 0 &&  x - 3 >= 0 &&
+    return y - 3 >= 0 &&  x - 3 >= 0 &&
      grid[y - 1][x - 1] === 'M' && grid[y - 2][x - 2] === 'A' && grid[y - 3][x - 3] === 'S';
   }
 
   const checkLeftDown = (x, y) => {
-    return  y + 3 < grid.length && x - 3 >= 0 &&
+    return y + 3 < grid.length && x - 3 >= 0 &&
      grid[y + 1][x - 1] === 'M' && grid[y + 2][x - 2] === 'A' && grid[y + 3][x - 3] === 'S';
   }
 
   const checkRightUp = (x, y) => {
-    return  y - 3 >= 0 && x + 3 < grid[0].length &&
+    return y - 3 >= 0 && x + 3 < grid[0].length &&
      grid[y - 1][x + 1] === 'M' && grid[y - 2][x + 2] === 'A' && grid[y - 3][x + 3] === 'S';
   }
 
+  // grid.forEach((row, y) => {
+  //   row.forEach((char, x) => {
+  //     if (char === 'X') {
+  //       if (checkRight(x, y)) total++;
+  //       if (checkLeft(x, y)) total++;
+  //       if (checkDown(x, y)) total++;
+  //       if (checkUp(x, y)) total++;
+  //       if (checkRightDown(x, y)) total++;
+  //       if (checkLeftUp(x, y)) total++;
+  //       if (checkLeftDown(x, y)) total++;
+  //       if (checkRightUp(x, y)) total++;
+  //     }
+  //   })
+  // })
 
-  //lignes
+
+  //2e partie
+
+  const rightDown = (x, y) => {
+    return x - 1 >= 0 && y - 1 >= 0 && y + 1 < grid.length &&
+     grid[y - 1][x - 1] === "M" && grid[y + 1][x + 1] === "S";
+  };
+
+  const leftDown = (x, y) => {
+    return x + 1 < grid[0].length && y - 1 >= 0 && y + 1 < grid.length &&
+     grid[y - 1][x + 1] === "M" && grid[y + 1][x - 1] === "S";
+  };
+
+  const rightUp = (x, y) => {
+    return  x - 1 < grid[0].length && y + 1 < grid.length &&
+     grid[y + 1][x - 1] === "M" && grid[y - 1][x + 1] === "S";
+  };
+
+  const leftUp = (x, y) => {
+    return y - 1 >= 0 && x + 1 < grid[0].length &&
+     grid[y + 1][x + 1] === "M" && grid[y - 1][x - 1] === "S";
+  };
+
+
   grid.forEach((row, y) => {
     row.forEach((char, x) => {
-      if (char === 'X') {
-        if (checkRight(x, y)) total++;
-        if (checkLeft(x, y)) total++;
-        if (checkDown(x, y)) total++;
-        if (checkUp(x, y)) total++;
-        if (checkRightDown(x, y)) total++;
-        if (checkLeftUp(x, y)) total++;
-        if (checkLeftDown(x, y)) total++;
-        if (checkRightUp(x, y)) total++;
+      if (char === "A") {
+        // if (rightDown(x, y) && leftDown(x, y)) total++;
+        // if (rightUp(x, y) && leftUp(x, y)) total++;
+        if (leftDown(x, y)) total++;
       }
     })
   })
+
   console.log(total);
+
 }
 
-findXmas(input);
+findXmas(grid);
